25fb24c02b5c053848202980a399896a9bc196be
Rubens <rubenssegundo404@gmail.com>
2025-11-21 16:06:50 -0300

    chore: license headers
    Signed-off-by: Rubens <rubenssegundo404@gmail.com>

--- internal/integrations/tflint/tflint.go
+++ internal/integrations/tflint/tflint.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2024 santosr2
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 // Package tflint implements the tflint integration for updating plugin versions in .tflint.hcl files.
 // It detects tflint configuration files, parses HCL to extract plugin versions, queries GitHub Releases
 // for plugin updates, and rewrites versions while preserving HCL formatting.
